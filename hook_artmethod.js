69c410f78e31b54c94bcba7cc8100dccbe160e77
Yang <yang@woshiyang.com>
2021-06-02 00:23:10 +0800

    update hook_artmethod

--- hook_artmethod.js
+++ hook_artmethod.js
@@ -1,9 +1,92 @@
 
+const STD_STRING_SIZE = 3 * Process.pointerSize;
+class StdString {
+    constructor() {
+        this.handle = Memory.alloc(STD_STRING_SIZE);
+    }
+
+    dispose() {
+        const [data, isTiny] = this._getData();
+        if (!isTiny) {
+            Java.api.$delete(data);
+        }
+    }
+
+    disposeToString() {
+        const result = this.toString();
+        this.dispose();
+        return result;
+    }
+
+    toString() {
+        const [data] = this._getData();
+        return data.readUtf8String();
+    }
+
+    _getData() {
+        const str = this.handle;
+        const isTiny = (str.readU8() & 1) === 0;
+        const data = isTiny ? str.add(1) : str.add(2 * Process.pointerSize).readPointer();
+        return [data, isTiny];
+    }
+}
+
+function prettyMethod(method_id, withSignature) {
+    const result = new StdString();
+    Java.api['art::ArtMethod::PrettyMethod'](result, method_id, withSignature ? 1 : 0);
+    return result.disposeToString();
+}
+
+function hook_dlopen(module_name , fun ) {
+    var android_dlopen_ext = Module.findExportByName(null, "android_dlopen_ext");
+
+    if (android_dlopen_ext) {
+        Interceptor.attach(android_dlopen_ext, {
+            onEnter: function (args) {
+                var pathptr = args[0];
+                if (pathptr) {
+                    this.path = (pathptr).readCString();
+                    if (this.path.indexOf(module_name) >= 0) {
+                        this.canhook = true;
+                        console.log("android_dlopen_ext:", this.path);
+                    }
+                }
+            },
+            onLeave: function (retval) {
+                if (this.canhook) {
+                    fun();
+                }
+            }
+        });
+    }
+    var dlopen = Module.findExportByName(null, "dlopen");
+    if (dlopen) {
+        Interceptor.attach(dlopen, {
+            onEnter: function (args) {
+                var pathptr = args[0];
+                if (pathptr) {
+                    this.path = (pathptr).readCString();
+                    if (this.path.indexOf(module_name) >= 0) {
+                        this.canhook = true;
+                        console.log("dlopen:", this.path);
+                    }
+                }
+            },
+            onLeave: function (retval) {
+                if (this.canhook) {
+                    fun();
+                }
+            }
+        });
+    }
+    console.log("android_dlopen_ext:", android_dlopen_ext, "dlopen:", dlopen);
+}
+
+
 function hook_native() {
     var module_libart = Process.findModuleByName("libart.so");
     var symbols = module_libart.enumerateSymbols();
     var ArtMethod_Invoke = null;
-    var ArtMethod_PrettyMethod = null;
     for (var i = 0; i < symbols.length; i++) {
         var symbol = symbols[i];
         var address = symbol.address;
@@ -19,46 +102,21 @@ function hook_native() {
             console.log(name);
             ArtMethod_Invoke = address;
         }
-
-        if (indexArtMethod >= 0 && name.indexOf("PrettyMethod") >= 0 && name.indexOf("Eb") >= 0) {
-            console.log(name);
-            ArtMethod_PrettyMethod = address;
-        }
     }
-
-    var module_libext = null;
-    if (Process.arch === "arm64") {
-        module_libext = Module.load("/data/app/libext64.so");
-    } else if (Process.arch === "arm") {
-        module_libext = Module.load("/data/app/libext.so");
-    }
-    if (module_libext != null) {
-        var addr_PrettyMethod = module_libext.findExportByName("PrettyMethod");
-        var PrettyMethod = new NativeFunction(addr_PrettyMethod, "void", ["pointer", "pointer", "pointer", "int"]);
-
-        if (ArtMethod_Invoke) {
-            var foo_ArtMethod_PrettyMethod = new NativeFunction(ArtMethod_PrettyMethod, "pointer", ["pointer", "int"]);
-            console.log(foo_ArtMethod_PrettyMethod);
-            Interceptor.attach(ArtMethod_Invoke, {
-                onEnter: function (args) {
-                    try {
-                        var result = Memory.alloc(0x100);
-                        PrettyMethod(ArtMethod_PrettyMethod, args[0], result, 0x100);
-                        console.log(result.readCString());
-
-                    } catch (error) {
-                        console.log(error);
-                    }
-
-                }, onLeave: function (retval) {
-
+    if (ArtMethod_Invoke) {
+        Interceptor.attach(ArtMethod_Invoke, {
+            onEnter: function (args) {
+                var method_name = prettyMethod(args[0], 0);
+                if (!(method_name.indexOf("java.") == 0 || method_name.indexOf("android.") == 0)) {
+                    console.log("ArtMethod Invoke:", method_name);
                 }
-            });
-        }
+            }
+        });
     }
 }
 
 function main() {
+    hook_dlopen("libart.so", hook_native);
     hook_native();
 }
 
