c42bb2e0beb00f51b13a9482641fbeda0e2c4dba
yang <yang@woshiyang.com>
2022-08-03 18:19:03 +0800

    update hook_RegisterNatives.js

--- hook_RegisterNatives.js
+++ hook_RegisterNatives.js
@@ -1,9 +1,9 @@
 
 function find_RegisterNatives(params) {
-    var symbols = Module.enumerateSymbolsSync("libart.so");
-    var addrRegisterNatives = null;
-    for (var i = 0; i < symbols.length; i++) {
-        var symbol = symbols[i];
+    let symbols = Module.enumerateSymbolsSync("libart.so");
+    let addrRegisterNatives = null;
+    for (let i = 0; i < symbols.length; i++) {
+        let symbol = symbols[i];
         
         //_ZN3art3JNI15RegisterNativesEP7_JNIEnvP7_jclassPK15JNINativeMethodi
         if (symbol.name.indexOf("art") >= 0 &&
@@ -24,24 +24,22 @@ function hook_RegisterNatives(addrRegisterNatives) {
         Interceptor.attach(addrRegisterNatives, {
             onEnter: function (args) {
                 console.log("[RegisterNatives] method_count:", args[3]);
-                var env = args[0];
-                var java_class = args[1];
-                var class_name = Java.vm.tryGetEnv().getClassName(java_class);
+                let java_class = args[1];
+                let class_name = Java.vm.tryGetEnv().getClassName(java_class);
                 //console.log(class_name);
 
-                var methods_ptr = ptr(args[2]);
+                let methods_ptr = ptr(args[2]);
 
-                var method_count = parseInt(args[3]);
-                for (var i = 0; i < method_count; i++) {
-                    var name_ptr = Memory.readPointer(methods_ptr.add(i * Process.pointerSize * 3));
-                    var sig_ptr = Memory.readPointer(methods_ptr.add(i * Process.pointerSize * 3 + Process.pointerSize));
-                    var fnPtr_ptr = Memory.readPointer(methods_ptr.add(i * Process.pointerSize * 3 + Process.pointerSize * 2));
+                let method_count = parseInt(args[3]);
+                for (let i = 0; i < method_count; i++) {
+                    let name_ptr = Memory.readPointer(methods_ptr.add(i * Process.pointerSize * 3));
+                    let sig_ptr = Memory.readPointer(methods_ptr.add(i * Process.pointerSize * 3 + Process.pointerSize));
+                    let fnPtr_ptr = Memory.readPointer(methods_ptr.add(i * Process.pointerSize * 3 + Process.pointerSize * 2));
 
-                    var name = Memory.readCString(name_ptr);
-                    var sig = Memory.readCString(sig_ptr);
-                    var find_module = Process.findModuleByAddress(fnPtr_ptr);
-                    console.log("[RegisterNatives] java_class:", class_name, "name:", name, "sig:", sig, "fnPtr:", fnPtr_ptr,  " fnOffset:", ptr(fnPtr_ptr).sub(find_module.base), " callee:", DebugSymbol.fromAddress(this.returnAddress));
-
+                    let name = Memory.readCString(name_ptr);
+                    let sig = Memory.readCString(sig_ptr);
+                    let symbol = DebugSymbol.fromAddress(fnPtr_ptr)
+                    console.log("[RegisterNatives] java_class:", class_name, "name:", name, "sig:", sig, "fnPtr:", fnPtr_ptr,  " fnOffset:", symbol, " callee:", DebugSymbol.fromAddress(this.returnAddress));
                 }
             }
         });
